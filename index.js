6747d427722ea53058ed0c2d9e7d426cfa9262fb
philemonmurangiri <murangirilesx890@gmail.com>
2024-03-31 20:52:41 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your code here!
+
+const mainElementBefore = document.querySelector('main#main');
+
+
+if (mainElementBefore) {
+  console.error("The main#main element exists before processing index.js");
+} else {
+  console.log("The main#main element doesn't exist before processing index.js");
+}
+
+
+const mainElement = document.querySelector('main#main');
+if (mainElement) {
+  mainElement.remove();
+}
+
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+const yourName = "Philemon Murangiri"; 
+newHeader.textContent = `${yourName} is the champion`;
+
+
+document.body.appendChild(newHeader);
